3ee372d48250f68fcafb9268f270c251cbc1fab2
demandsupply <giova.zol92@gmail.com>
2025-03-26 18:20:30 +0100

    add function which compares the human and computer choiches and assign one point to the winner

--- javascript.js
+++ javascript.js
@@ -3,17 +3,45 @@
 // execute the random function
 // compare the results and prnit if the user won or lost
 
+let humanScore = 0;
+let computerScore = 0;
 const possibleOutputs = ["rock", "paper", "scissor"]
 
 function getComputerChoice() {
     return possibleOutputs[Math.floor(Math.random() * 10) % possibleOutputs.length]
 }
 
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let promptHuman = prompt("Rock, paper or scissor? ")
+    let promptHuman = prompt("rock, paper or scissor? ")
     return promptHuman;
 }
 
-console.log(getHumanChoice());
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    console.log(`human choice is ${humanChoice}, computer choice is ${computerChoice}`)
+
+    if (
+        (humanChoice === possibleOutputs[0] && computerChoice === possibleOutputs[1]) || 
+        (humanChoice === possibleOutputs[1] && computerChoice === possibleOutputs[2]) || 
+        (humanChoice === possibleOutputs[2] && computerChoice === possibleOutputs[0])) {
+            console.log("human lost")
+            computerScore++;
+        } else if (
+        (humanChoice === possibleOutputs[0] && computerChoice === possibleOutputs[2]) || 
+        (humanChoice === possibleOutputs[1] && computerChoice === possibleOutputs[0]) || 
+        (humanChoice === possibleOutputs[2] && computerChoice === possibleOutputs[1])) {
+            console.log("computer lost")
+            humanScore++;
+        } else {
+            console.log("tie")
+    }
+    console.log(`human score: ${humanScore}, computer score: ${computerScore}`);
+
+}
+
+
+playRound();
