a0b2f2423d293220be81f768408868bb1e765060
demandsupply <giova.zol92@gmail.com>
2025-03-27 09:08:41 +0100

    add function which plays 5 rounds and declare a winner or tie game

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 // write a function that randomly returns "rock", "paper", "scissor"
 // prompt user for "rock", "paper", "scissor"
 // execute the random function
-// compare the results and prnit if the user won or lost
+// compare the results and print if the user won or lost
+// play the game 5 times and output the final winner
 
 let humanScore = 0;
 let computerScore = 0;
@@ -13,14 +14,12 @@ function getComputerChoice() {
 
 
 function getHumanChoice() {
-    let promptHuman = prompt("rock, paper or scissor? ")
+    let promptHuman = prompt("rock, paper or scissor? ");
     return promptHuman;
 }
 
 
-function playRound() {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
+function playRound(humanChoice, computerChoice) {
 
     console.log(`human choice is ${humanChoice}, computer choice is ${computerChoice}`)
 
@@ -28,20 +27,37 @@ function playRound() {
         (humanChoice === possibleOutputs[0] && computerChoice === possibleOutputs[1]) || 
         (humanChoice === possibleOutputs[1] && computerChoice === possibleOutputs[2]) || 
         (humanChoice === possibleOutputs[2] && computerChoice === possibleOutputs[0])) {
-            console.log("human lost")
+            console.log(`You lost!`)
             computerScore++;
         } else if (
         (humanChoice === possibleOutputs[0] && computerChoice === possibleOutputs[2]) || 
         (humanChoice === possibleOutputs[1] && computerChoice === possibleOutputs[0]) || 
         (humanChoice === possibleOutputs[2] && computerChoice === possibleOutputs[1])) {
-            console.log("computer lost")
+            console.log("You won!")
             humanScore++;
         } else {
-            console.log("tie")
+            console.log("Tie...")
     }
     console.log(`human score: ${humanScore}, computer score: ${computerScore}`);
 
 }
 
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice().toLowerCase();
+        let computerChoice = getComputerChoice();
 
-playRound();
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (computerScore > humanScore) {
+        alert(`Winner is the computer with ${computerScore} points vs your ${humanScore} points!`);
+    } else if (computerScore < humanScore) {
+        alert(`You are the winner with ${humanScore} points vs ${computerScore} computer's points!`);
+    } else {
+        alert(`You both have ${humanScore} points. Result is tie!`)
+    }
+    return
+}
+
+playGame();
